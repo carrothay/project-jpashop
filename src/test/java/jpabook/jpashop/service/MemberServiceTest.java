3488ce1f313127efd6337f203aabe3f47cb2e211
carrothay <hazel.hykim@gmail.com>
2022-10-05 23:43:43 +0800

    commit 05/10/22

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,10 +1,12 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,17 +21,32 @@ public class MemberServiceTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    //@Rollback(false) //자동 롤백 안되게 테스트 설정
     public void 회원가입() throws Exception {
         //given
+        Member member = new Member();
+        member.setName("kim");
         //when
+        Long savedId = memberService.join(member);
         //then
-
+        assertEquals(member, memberRepository.findOne(savedId));
     }
 
-    @Test
-    public void 중복_회원_예외() throws Exception {
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception{
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
         //when
+        memberService.join(member1);
+            memberService.join(member2); //여기서 예외가 발생해야 함
+
         //then
+        fail("예외가 발생해야 한다.");
     }
+
 }
